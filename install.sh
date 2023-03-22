e512b018b429dcc2e3969c3ac3a8dea7cb560775
FII14 <121723975+FII14@users.noreply.github.com>
2023-03-22 22:17:39 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Install Driver Wifi Realtek RTL8188EUS di Kali Linux
+
+install_driver(){
+        apt-get install bc
+        apt-get install git
+        apt-get install build-essential
+        apt-get install libelf-dev
+	apt-get install linux-headers-$(uname -r)
+        git clone https://github.com/KanuX-14/rtl8188eus.git
+        cd rtl8188eus
+        echo "blacklist r8188eu" > "/etc/modprobe.d/realtek.conf"
+        make
+        make install
+        modprobe 8188eu
+	echo "Reebot Kali Linux, dan jalankan script mode_monitor.sh"
+	exit 0
+}
+
+while true
+do
+        read -p "Apakah anda mau menginstall Driver Wifi Realtek RTL8188EUS [iya/tidak]: " nanya
+
+        if [[ "${nanya}" == "iya" ]]; then
+                install_driver
+                exit 0
+
+        else
+                echo "Masukan salah!"
+                continue
+        fi
+
+done
